7a39125bc4da0191429f3409b2d60780cb7d02e7
BenWoodworth <ben@benwoodworth.net>
2019-11-04 08:56:25 -0500

    Rename UserInfoScope to GetUserInfoScope.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/GetUserInfoScope.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/GetUserInfoScope.kt
@@ -3,7 +3,7 @@ package net.benwoodworth.groupme.client
 import net.benwoodworth.groupme.User
 import net.benwoodworth.groupme.UserInfo
 
-interface UserInfoScope {
+interface GetUserInfoScope {
     suspend fun getUserInfo(user: User): UserInfo
 
     suspend fun User.getInfo(): UserInfo
--- src/commonMain/kotlin/net/benwoodworth/groupme/client/GroupMeClient.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/GroupMeClient.kt
@@ -2,7 +2,7 @@ package net.benwoodworth.groupme.client
 
 import net.benwoodworth.groupme.GroupMeDsl
 
-interface GroupMeClient : UserInfoScope, GetChatClientScope {
+interface GroupMeClient : GetUserInfoScope, GetChatClientScope {
     val authenticatedUser: AuthenticatedUser
 
     suspend operator fun invoke(@GroupMeDsl block: suspend GroupMeClient.() -> Unit): GroupMeClient
