34020dd00fa11fd0f1370f83a4452c8f9d6f1f0d
BenWoodworth <ben@benwoodworth.net>
2019-11-07 15:36:55 -0500

    Implement DefaultHttpClient for javascript.

---
+++ src/jsMain/kotlin/net/benwoodworth/groupme/api/DefaultHttpClient.kt
@@ -0,0 +1,41 @@
+package net.benwoodworth.groupme.api
+
+import org.w3c.xhr.XMLHttpRequest
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+internal actual class DefaultHttpClient actual constructor() : HttpClient {
+    override suspend fun sendRequest(
+        method: String,
+        url: String,
+        headers: Map<String, String?>,
+        params: Map<String, String?>,
+        body: String?
+    ): HttpResponse {
+        return suspendCoroutine { continuation ->
+            val request = XMLHttpRequest()
+
+            request.onload = {
+                continuation.resume(
+                    HttpResponse(
+                        request.status.toInt(),
+                        request.statusText,
+                        request.responseText
+                    )
+                )
+            }
+
+            headers.forEach { (key, value) ->
+                value?.let { request.setRequestHeader(key, value) }
+            }
+
+            request.send()
+        }
+    }
+
+    override fun urlEncode(string: String): String {
+        return encodeURIComponent(string)
+    }
+}
+
+private external fun encodeURIComponent(str: String): String
