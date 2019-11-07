b867ec871781e92dad8805a94aba21cbef7c0050
BenWoodworth <ben@benwoodworth.net>
2019-11-07 13:59:10 -0500

    Mark MessageLikingScope and MessagingScope with @GroupMeScope.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/MessageLikingScope.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/MessageLikingScope.kt
@@ -1,5 +1,8 @@
 package net.benwoodworth.groupme.client.chat
 
+import net.benwoodworth.groupme.GroupMeScope
+
+@GroupMeScope
 interface MessageLikingScope {
     suspend fun Message.like()
 
--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/MessagingScope.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/MessagingScope.kt
@@ -1,5 +1,8 @@
 package net.benwoodworth.groupme.client.chat
 
+import net.benwoodworth.groupme.GroupMeScope
+
+@GroupMeScope
 interface MessagingScope : MessageLikingScope {
     suspend fun sendMessage(message: Message): SentMessage
 
