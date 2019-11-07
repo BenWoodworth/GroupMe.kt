fb9270b0e627b393d45eee6faea9e41c9457ac66
BenWoodworth <ben@benwoodworth.net>
2019-11-07 13:58:44 -0500

    Make GroupMeClient a MessageLikingScope.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/GroupMeClient.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/GroupMeClient.kt
@@ -1,9 +1,10 @@
 package net.benwoodworth.groupme.client
 
 import net.benwoodworth.groupme.GroupMeScope
+import net.benwoodworth.groupme.client.chat.MessageLikingScope
 
 @GroupMeScope
-interface GroupMeClient : GetUserInfoScope, GetChatClientScope {
+interface GroupMeClient : GetUserInfoScope, GetChatClientScope, MessageLikingScope {
     val authenticatedUser: AuthenticatedUser
 
     suspend operator fun invoke(block: suspend GroupMeClient.() -> Unit): GroupMeClient
