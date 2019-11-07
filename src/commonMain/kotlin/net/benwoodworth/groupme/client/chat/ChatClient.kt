07ef7c5c9a3e9dab57446665769bfcc3ae1e5f32
BenWoodworth <ben@benwoodworth.net>
2019-11-07 13:58:22 -0500

    Make ChatClient a MessagingScope.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/ChatClient.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/ChatClient.kt
@@ -3,7 +3,7 @@ package net.benwoodworth.groupme.client.chat
 import net.benwoodworth.groupme.GroupMeScope
 
 @GroupMeScope
-interface ChatClient {
+interface ChatClient : MessagingScope {
     val chat: Chat
 
     suspend operator fun invoke(block: suspend ChatClient.() -> Unit): ChatClient
