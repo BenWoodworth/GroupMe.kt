82fd32949915c4f23c704880b7d4d9c6668cca27
BenWoodworth <ben@benwoodworth.net>
2019-11-08 11:10:57 -0500

    Start implementing GetUserInfoScope.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/GetUserInfoScope.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/GetUserInfoScope.kt
@@ -1,12 +1,61 @@
 package net.benwoodworth.groupme.client
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import net.benwoodworth.groupme.User
 import net.benwoodworth.groupme.UserInfo
+import net.benwoodworth.groupme.api.GroupMeHttpClient
+import net.benwoodworth.groupme.api.HttpMethod
+import net.benwoodworth.groupme.api.ResponseEnvelope
+import net.benwoodworth.groupme.client.media.GroupMeImage
 
 interface GetUserInfoScope {
     suspend fun getUserInfo(user: User): UserInfo
 
-    suspend fun User.getInfo(): UserInfo
+    suspend fun User.getInfo(): UserInfo {
+        return getUserInfo(this)
+    }
 
     suspend fun getAuthenticatedUserInfo(): AuthenticatedUserInfo
+}
+
+internal class GetUserInfoScopeImpl(
+    private val httpClient: GroupMeHttpClient,
+    private val json: Json
+) : GetUserInfoScope {
+    override suspend fun getUserInfo(user: User): UserInfo {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun getAuthenticatedUserInfo(): AuthenticatedUserInfo {
+        val response = httpClient.sendApiV3Request(
+            method = HttpMethod.Get,
+            endpoint = "/users/me"
+        )
+
+        val responseData = json.parse(
+            deserializer = ResponseEnvelope.serializer(AuthenticatedUserInfoResponse.serializer()),
+            string = response.data
+        )
+
+        return responseData.response!!.run {
+            AuthenticatedUserInfo(
+                userId = id,
+                name = name,
+                avatar = GroupMeImage(image_url)
+            )
+        }
+    }
+
+    @Serializable
+    private class AuthenticatedUserInfoResponse(
+        val id: String,
+        val phone_number: String,
+        val image_url: String,
+        val name: String,
+        val created_at: Long,
+        val updated_at: Long,
+        val email: String,
+        val sms: Boolean
+    )
 }
