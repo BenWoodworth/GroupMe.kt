d0bb2aa6e638c3e48fd93d757484f9dae179b3cf
BenWoodworth <ben@benwoodworth.net>
2019-11-08 09:32:40 -0500

    Add httpClient to GroupMe.getClient().

--- src/commonMain/kotlin/net/benwoodworth/groupme/GroupMe.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/GroupMe.kt
@@ -1,9 +1,14 @@
 package net.benwoodworth.groupme
 
+import net.benwoodworth.groupme.api.DefaultHttpClient
+import net.benwoodworth.groupme.api.HttpClient
 import net.benwoodworth.groupme.client.GroupMeClient
 
 object GroupMe {
-    suspend fun getClient(apiToken: String): GroupMeClient {
+    suspend fun getClient(
+        apiToken: String,
+        httpClient: HttpClient = DefaultHttpClient()
+    ): GroupMeClient {
         TODO()
     }
 }
