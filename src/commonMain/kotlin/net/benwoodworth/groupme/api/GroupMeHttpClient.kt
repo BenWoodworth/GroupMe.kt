f26c44114c55772f323f7f2b0920037cfa5f4495
BenWoodworth <ben@benwoodworth.net>
2019-11-08 10:08:56 -0500

    Add GroupMeHttpClient.sendApiV3Request().

--- src/commonMain/kotlin/net/benwoodworth/groupme/api/GroupMeHttpClient.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/api/GroupMeHttpClient.kt
@@ -2,7 +2,8 @@ package net.benwoodworth.groupme.api
 
 internal class GroupMeHttpClient(
     private val httpClient: HttpClient,
-    private val apiKey: String
+    apiKey: String,
+    private val apiV3UrlBase: String
 ) : HttpClient by httpClient {
     private val accessTokenHeader = "X-ACCESS-TOKEN" to apiKey
 
@@ -12,11 +13,27 @@ internal class GroupMeHttpClient(
         headers: Map<String, String?>,
         params: Map<String, String?>,
         body: String?
-    ): HttpResponse {
+    ): HttpClient.Response {
         return httpClient.sendRequest(
             method = method,
             url = url,
             headers = headers + accessTokenHeader,
+            params = params,
+            body = body
+        )
+    }
+
+    suspend fun sendApiV3Request(
+        method: String,
+        endpoint: String,
+        headers: Map<String, String?> = emptyMap(),
+        params: Map<String, String?> = emptyMap(),
+        body: String? = null
+    ): HttpClient.Response {
+        return sendRequest(
+            method = method,
+            url = "$apiV3UrlBase$endpoint",
+            headers = headers,
             params = params,
             body = body
         )
