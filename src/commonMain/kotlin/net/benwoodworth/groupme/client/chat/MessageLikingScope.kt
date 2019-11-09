1809cd704f6cd56ade22ee574611d7fe6091de27
BenWoodworth <ben@benwoodworth.net>
2019-11-08 22:26:35 -0500

    Create implementation for MessageLikingScope.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/MessageLikingScope.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/MessageLikingScope.kt
@@ -1,10 +1,25 @@
 package net.benwoodworth.groupme.client.chat
 
+import kotlinx.serialization.json.Json
 import net.benwoodworth.groupme.GroupMeScope
+import net.benwoodworth.groupme.api.GroupMeHttpClient
 
 @GroupMeScope
 interface MessageLikingScope {
     suspend fun Message.like()
 
     suspend fun Message.unlike()
+}
+
+internal class MessageLikingScopeImpl(
+    private val httpClient: GroupMeHttpClient,
+    private val json: Json
+) : MessageLikingScope {
+    override suspend fun Message.like() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun Message.unlike() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
