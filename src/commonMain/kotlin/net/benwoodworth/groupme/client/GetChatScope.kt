3aed39bb6882c5a25000614679d8499f0ff5efc2
BenWoodworth <ben@benwoodworth.net>
2019-11-08 22:27:30 -0500

    Create implementation for GetChatScope.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/GetChatScope.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/GetChatScope.kt
@@ -1,13 +1,12 @@
 package net.benwoodworth.groupme.client
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.serialization.json.Json
 import net.benwoodworth.groupme.GroupMeScope
+import net.benwoodworth.groupme.api.GroupMeHttpClient
 import net.benwoodworth.groupme.client.chat.Chat
-import net.benwoodworth.groupme.client.chat.ChatClient
 import net.benwoodworth.groupme.client.chat.direct.DirectChat
-import net.benwoodworth.groupme.client.chat.direct.DirectChatClient
 import net.benwoodworth.groupme.client.chat.group.GroupChat
-import net.benwoodworth.groupme.client.chat.group.GroupChatClient
 
 @GroupMeScope
 interface GetChatScope {
@@ -16,4 +15,21 @@ interface GetChatScope {
     fun getDirectChats(): Flow<DirectChat>
 
     fun getGroupChats(): Flow<GroupChat>
+}
+
+internal class GetChatScopeImpl(
+    private val httpClient: GroupMeHttpClient,
+    private val json: Json
+) : GetChatScope {
+    override fun getChats(): Flow<Chat> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getDirectChats(): Flow<DirectChat> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getGroupChats(): Flow<GroupChat> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
