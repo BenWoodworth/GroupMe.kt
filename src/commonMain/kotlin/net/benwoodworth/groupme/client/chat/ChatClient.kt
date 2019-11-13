1d6af042d0d0503b46009d7df4cff1cf1312fcb7
BenWoodworth <ben@benwoodworth.net>
2019-11-13 13:03:57 -0500

    Delete MessagingScope interface.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/ChatClient.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/ChatClient.kt
@@ -1,8 +1,23 @@
 package net.benwoodworth.groupme.client.chat
 
+import kotlinx.coroutines.flow.Flow
 import net.benwoodworth.groupme.GroupMeScope
 
 @GroupMeScope
-interface ChatClient : MessagingScope {
+interface ChatClient {
     val chat: Chat
+
+    suspend fun sendMessage(message: Message): SentMessage
+
+    suspend fun Message.send(): SentMessage {
+        return sendMessage(this)
+    }
+
+    fun getMessages(): Flow<SentMessage>
+
+    fun getMessagesBefore(before: SentMessage): Flow<SentMessage>
+
+    fun getMessagesSince(since: SentMessage): Flow<SentMessage>
+
+    fun getMessagesAfter(after: SentMessage): Flow<SentMessage>
 }
