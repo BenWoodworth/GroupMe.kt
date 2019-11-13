052306eaa19dcdefb07922f0981eeb4fa8d8a7b3
BenWoodworth <ben@benwoodworth.net>
2019-11-13 14:44:42 -0500

    Implement GroupChatClient.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/group/GroupChatClient.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/group/GroupChatClient.kt
@@ -7,3 +7,9 @@ import net.benwoodworth.groupme.client.chat.ChatClient
 interface GroupChatClient : ChatClient, GroupMessagingScope {
     override val chat: GroupChat
 }
+
+internal class GroupChatClientImpl(
+    override val chat: GroupChat,
+    groupMessagingScope: GroupMessagingScope
+) : ChatClient,
+    GroupMessagingScope by groupMessagingScope
