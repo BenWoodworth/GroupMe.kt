8a8c59b508b5d3068b7d60c1b9d13294506597a1
BenWoodworth <ben@benwoodworth.net>
2019-11-13 15:11:30 -0500

    Implement DirectSentMessageInfo.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/direct/DirectSentMessageInfo.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/direct/DirectSentMessageInfo.kt
@@ -1,6 +1,14 @@
 package net.benwoodworth.groupme.client.chat.direct
 
+import kotlinx.serialization.json.JsonObject
 import net.benwoodworth.groupme.client.chat.SentMessageInfo
+import net.benwoodworth.groupme.client.chat.SentMessageInfoBase
 
 interface DirectSentMessageInfo : SentMessageInfo {
+    override val chat: DirectChat
 }
+
+internal class DirectSentMessageInfoImpl(
+    messageJson: JsonObject,
+    override val chat: DirectChat
+) : SentMessageInfoBase(messageJson), DirectSentMessageInfo
