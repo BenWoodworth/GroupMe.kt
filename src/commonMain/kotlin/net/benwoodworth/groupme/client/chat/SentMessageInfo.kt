07b0e0e9f562fc110bcc59dfa165ab87998b638c
BenWoodworth <ben@benwoodworth.net>
2019-11-13 15:11:02 -0500

    Create SentMessageInfoBase, and refactor GroupSentMessageInfoImpl.

--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/SentMessageInfo.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/SentMessageInfo.kt
@@ -11,3 +11,19 @@ interface SentMessageInfo : SentMessage {
     val attachments: List<Attachment>
     val sourceGuid: String
 }
+
+internal abstract class SentMessageInfoBase(
+    final override val messageJson: JsonObject
+) : SentMessageInfo {
+    override val messageId: String = messageJson.getPrimitive("id").content
+
+    override val sender: User = User(messageJson.getPrimitive("user_id").content)
+
+    override val text: String? = messageJson.getPrimitive("text").contentOrNull
+
+    override val attachments: List<Attachment> = messageJson.getArray("attachments").map {
+        Attachment(it.jsonObject)
+    }
+
+    override val sourceGuid: String = messageJson.getPrimitive("source_guid").content
+}
--- src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/group/GroupSentMessageInfo.kt
+++ src/commonMain/kotlin/net/benwoodworth/groupme/client/chat/group/GroupSentMessageInfo.kt
@@ -1,28 +1,14 @@
 package net.benwoodworth.groupme.client.chat.group
 
 import kotlinx.serialization.json.JsonObject
-import net.benwoodworth.groupme.User
-import net.benwoodworth.groupme.client.chat.Attachment
 import net.benwoodworth.groupme.client.chat.SentMessageInfo
+import net.benwoodworth.groupme.client.chat.SentMessageInfoBase
 
 interface GroupSentMessageInfo : SentMessageInfo {
     override val chat: GroupChat
 }
 
-class GroupSentMessageInfoImpl(
-    override val messageJson: JsonObject
-) : GroupSentMessageInfo {
-    override val messageId: String = messageJson.getPrimitive("id").content
-
-    override val sender: User = User(messageJson.getPrimitive("user_id").content)
-
-    override val chat: GroupChat = GroupChat(messageJson.getPrimitive("group_id").content)
-
-    override val text: String? = messageJson.getPrimitive("text").contentOrNull
-
-    override val attachments: List<Attachment> = messageJson.getArray("attachments").map {
-        Attachment(it.jsonObject)
-    }
-
-    override val sourceGuid: String = messageJson.getPrimitive("source_guid").content
-}
+internal class GroupSentMessageInfoImpl(
+    messageJson: JsonObject,
+    override val chat: GroupChat
+) : SentMessageInfoBase(messageJson), GroupSentMessageInfo
