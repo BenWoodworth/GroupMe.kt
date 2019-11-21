0c364354173642443b75db874cd499b79fc3c1df
BenWoodworth <ben@benwoodworth.net>
2019-11-21 17:15:19 -0500

    Implement functions in SentMessage.

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/SentMessage.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/SentMessage.kt
@@ -2,4 +2,19 @@ package net.benwoodworth.groupme.client.chat
 
 abstract class SentMessage internal constructor() {
     abstract val messageId: String
+
+    /**
+     * Compares [SentMessage]s by [messageId].
+     */
+    final override fun equals(other: Any?): Boolean {
+        return other is SentMessage && other.messageId == messageId
+    }
+
+    final override fun hashCode(): Int {
+        return messageId.hashCode()
+    }
+
+    override fun toString(): String {
+        return "SentMessage($messageId)"
+    }
 }
