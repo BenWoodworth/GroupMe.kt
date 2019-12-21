589fa74ff63551a37e2b75eb443e8f9626abfb73
BenWoodworth <ben@benwoodworth.net>
2019-12-21 08:39:04 -0500

    Make SentMessage extend Message.

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/SentMessageInfo.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/SentMessageInfo.kt
@@ -1,16 +1,11 @@
 package net.benwoodworth.groupme.client.chat
 
-import kotlinx.serialization.json.JsonObject
 import net.benwoodworth.groupme.User
 import net.benwoodworth.groupme.UserInfo
 
-interface SentMessageInfo : SentMessage {
+interface SentMessageInfo : SentMessage, Message {
     val chat: Chat
-    val json: JsonObject
     val sender: UserInfo
-    val text: String?
-    val attachments: List<Attachment>
-    val sourceGuid: String
     val likes: List<User>
     val created: Long
 }
