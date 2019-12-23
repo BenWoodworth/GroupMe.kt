0af508c9a62490d0a472a0816e09acd9dc9cf07a
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:35:35 -0500

    Merge DirectChat.kt and DirectChat.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/direct/DirectChat.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/direct/DirectChat.kt
@@ -7,3 +7,22 @@ interface DirectChat : Chat {
     val fromUser: User
     val toUser: User
 }
+
+private class DirectChatImpl(
+    override val fromUser: User,
+    override val toUser: User
+) : DirectChat, Chat by Chat(
+    chatId = "${fromUser.userId}+${toUser.userId}"
+) {
+    override fun toString(): String {
+        return "DirectChat($fromUser, $toUser)"
+    }
+}
+
+fun DirectChat(
+    fromUser: User,
+    toUser: User
+): DirectChat = DirectChat(
+    fromUser = fromUser,
+    toUser = toUser
+)
