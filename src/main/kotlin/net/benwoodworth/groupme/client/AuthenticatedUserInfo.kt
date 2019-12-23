350b31029b3b54f2f659151b6c2fd645bbe33ba5
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:34:22 -0500

    Merge SentMessageInfo.kt and SentMessageInfo.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/client/AuthenticatedUserInfo.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/AuthenticatedUserInfo.kt
@@ -2,7 +2,31 @@ package net.benwoodworth.groupme.client
 
 import kotlinx.serialization.json.JsonObject
 import net.benwoodworth.groupme.NamedUserInfo
+import net.benwoodworth.groupme.client.media.GroupMeImage
 
 interface AuthenticatedUserInfo : NamedUserInfo {
     val json: JsonObject
 }
+
+private class AuthenticatedUserInfoImpl(
+    override val json: JsonObject,
+    userId: String,
+    name: String,
+    avatar: GroupMeImage?
+) : AuthenticatedUserInfo, NamedUserInfo by NamedUserInfo(
+    userId = userId,
+    name = name,
+    avatar = avatar
+)
+
+internal fun AuthenticatedUserInfo(
+    json: JsonObject,
+    userId: String,
+    name: String,
+    avatar: GroupMeImage?
+): AuthenticatedUserInfo = AuthenticatedUserInfoImpl(
+    json = json,
+    userId = userId,
+    name = name,
+    avatar = avatar
+)
