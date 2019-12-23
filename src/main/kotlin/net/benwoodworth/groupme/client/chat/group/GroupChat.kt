2feb3dcf2c341acdf3b2c53a7cb1e76d0190bcf9
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:35:03 -0500

    Merge GroupChat.kt and GroupChat.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/group/GroupChat.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/group/GroupChat.kt
@@ -3,3 +3,19 @@ package net.benwoodworth.groupme.client.chat.group
 import net.benwoodworth.groupme.client.chat.Chat
 
 interface GroupChat : Chat
+
+private class GroupChatImpl(
+    chatId: String
+) : GroupChat, Chat by Chat(
+    chatId = chatId
+) {
+    override fun toString(): String {
+        return "GroupChat($chatId)"
+    }
+}
+
+fun GroupChat(
+    chatId: String
+): GroupChat = GroupChatImpl(
+    chatId = chatId
+)
