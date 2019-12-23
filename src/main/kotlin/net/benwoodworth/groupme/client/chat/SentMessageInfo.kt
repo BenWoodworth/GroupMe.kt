d037e5a3f59975e0435c54ab086a44f7b7ba4024
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:33:29 -0500

    Merge SentMessageInfo.kt and SentMessageInfo.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/SentMessageInfo.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/SentMessageInfo.kt
@@ -1,5 +1,6 @@
 package net.benwoodworth.groupme.client.chat
 
+import kotlinx.serialization.json.JsonObject
 import net.benwoodworth.groupme.User
 import net.benwoodworth.groupme.UserInfo
 
@@ -9,3 +10,39 @@ interface SentMessageInfo : SentMessage, Message {
     val likes: List<User>
     val created: Long
 }
+
+private class SentMessageInfoImpl(
+    override val chat: Chat,
+    override val json: JsonObject,
+    messageId: String,
+    override val sender: UserInfo,
+    override val text: String?,
+    override val attachments: List<Attachment>,
+    override val sourceGuid: String,
+    override val likes: List<User>,
+    override val created: Long
+) : SentMessageInfo, SentMessage by SentMessage(
+    messageId = messageId
+)
+
+internal fun SentMessageInfo(
+    chat: Chat,
+    json: JsonObject,
+    messageId: String,
+    sender: UserInfo,
+    text: String?,
+    attachments: List<Attachment>,
+    sourceGuid: String,
+    likes: List<User>,
+    created: Long
+): SentMessageInfo = SentMessageInfoImpl(
+    chat = chat,
+    json = json,
+    messageId = messageId,
+    sender = sender,
+    text = text,
+    attachments = attachments,
+    sourceGuid = sourceGuid,
+    likes = likes,
+    created = created
+)
