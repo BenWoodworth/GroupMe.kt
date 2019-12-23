b3ccf84aa0b1059d51f979342296babdb2a4380a
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:38:59 -0500

    Merge NamedUserInfo.kt and NamedUserInfo.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/NamedUserInfo.kt
+++ src/main/kotlin/net/benwoodworth/groupme/NamedUserInfo.kt
@@ -1,5 +1,30 @@
 package net.benwoodworth.groupme
 
+import net.benwoodworth.groupme.client.media.GroupMeImage
+
 interface NamedUserInfo : UserInfo {
     val name: String
 }
+
+private class NamedUserInfoImpl(
+    userId: String,
+    override val name: String,
+    nickname: String,
+    avatar: GroupMeImage?
+) : NamedUserInfo, UserInfo by UserInfo(
+    userId = userId,
+    nickname = nickname,
+    avatar = avatar
+)
+
+internal fun NamedUserInfo(
+    userId: String,
+    name: String,
+    nickname: String = name,
+    avatar: GroupMeImage?
+): NamedUserInfo = NamedUserInfoImpl(
+    userId = userId,
+    name = name,
+    nickname = nickname,
+    avatar = avatar
+)
