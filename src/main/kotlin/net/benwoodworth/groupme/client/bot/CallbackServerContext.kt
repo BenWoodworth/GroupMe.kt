2628c4987c2f814005fcf31ab5df42b60cf7ffa5
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:42:07 -0500

    Merge CallbackServerContext.kt and CallbackServerContext.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/client/bot/CallbackServerContext.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/bot/CallbackServerContext.kt
@@ -1,6 +1,8 @@
 package net.benwoodworth.groupme.client.bot
 
+import kotlinx.serialization.json.Json
 import net.benwoodworth.groupme.GroupMeScope
+import net.benwoodworth.groupme.api.GroupMeHttpClient
 import net.benwoodworth.groupme.client.chat.Message
 
 @GroupMeScope
@@ -23,4 +25,18 @@ interface CallbackServerContext {
     suspend fun Bot.sendMessage(message: Message) = sendBotMessage(this, message)
 
     suspend fun Message.send(bot: Bot) = sendBotMessage(bot, this)
+}
+
+internal class CallbackServerContextImpl(
+    private val server: CallbackServer,
+    private val httpClient: GroupMeHttpClient,
+    private val json: Json
+) : CallbackServerContext {
+    override suspend fun stopServer() {
+        server.stop()
+    }
+
+    override fun getBotContext(bot: Bot): BotContext {
+        return BotContext(bot, httpClient, json)
+    }
 }
