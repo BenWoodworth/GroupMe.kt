5db347321a1c1f253e710df0601044a6fdc1f50f
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:31:28 -0500

    Merge Message.kt and Message.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/Message.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/Message.kt
@@ -1,6 +1,8 @@
 package net.benwoodworth.groupme.client.chat
 
+import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.json
 import kotlin.random.Random
 import kotlin.random.nextULong
 
@@ -21,3 +23,32 @@ interface Message {
         }
     }
 }
+
+private class MessageImpl(
+    override val json: JsonObject,
+    override val text: String?,
+    override val attachments: List<Attachment>,
+    override val sourceGuid: String
+) : Message
+
+fun Message(json: JsonObject): Message = MessageImpl(
+    json = json,
+    text = json.getPrimitive("text").contentOrNull,
+    attachments = json.getArray("attachments").map { Attachment(it.jsonObject) },
+    sourceGuid = json.getPrimitive("source_guid").content
+)
+
+fun Message(
+    text: String? = null,
+    attachments: List<Attachment> = emptyList(),
+    sourceGuid: String = Message.generateSourceGuid()
+): Message = MessageImpl(
+    json = json {
+        "text" to text
+        "attachments" to JsonArray(attachments.map { it.json })
+        "source_guid" to sourceGuid
+    },
+    text = text,
+    attachments = attachments,
+    sourceGuid = sourceGuid
+)
