a6dcf38c43a2a83fb3a0b1ebca97e077fd290368
BenWoodworth <ben@benwoodworth.net>
2019-12-23 06:21:33 -0500

    Add Mention.contains() for Int and User.

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/Mention.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/Mention.kt
@@ -6,4 +6,15 @@ data class Mention(
     val user: User,
     val start: Int,
     val length: Int
-)
+) {
+    val end: Int
+        get() = start + length
+
+    operator fun contains(index: Int): Boolean {
+        return index in start until end
+    }
+
+    operator fun contains(user: User): Boolean {
+        return this.user == user
+    }
+}
