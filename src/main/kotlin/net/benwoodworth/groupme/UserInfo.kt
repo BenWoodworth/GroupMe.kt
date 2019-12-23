c5bd808413f654d00b231d595561d63dc3cff22d
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:40:27 -0500

    Merge UserInfo.kt and UserInfo.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/UserInfo.kt
+++ src/main/kotlin/net/benwoodworth/groupme/UserInfo.kt
@@ -6,3 +6,25 @@ interface UserInfo : User {
     val nickname: String
     val avatar: GroupMeImage?
 }
+
+private class UserInfoImpl(
+    userId: String,
+    override val nickname: String,
+    override val avatar: GroupMeImage?
+) : UserInfo, User by User(
+    userId = userId
+) {
+    override fun toString(): String {
+        return "User($nickname)"
+    }
+}
+
+internal fun UserInfo(
+    userId: String,
+    nickname: String,
+    avatar: GroupMeImage?
+): UserInfo = UserInfoImpl(
+    userId = userId,
+    nickname = nickname,
+    avatar = avatar
+)
