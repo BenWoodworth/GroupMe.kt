50098c646f050c633ba2d25c7e89b4aa37a18a4a
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:32:17 -0500

    Merge Chat.kt and Chat.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/client/chat/Chat.kt
+++ src/main/kotlin/net/benwoodworth/groupme/client/chat/Chat.kt
@@ -10,3 +10,25 @@ interface Chat {
 
     override fun hashCode(): Int
 }
+
+private class ChatImpl(
+    override val chatId: String
+) : Chat {
+    override fun equals(other: Any?): Boolean {
+        return other is Chat && chatId == other.chatId
+    }
+
+    override fun hashCode(): Int {
+        return chatId.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Chat($chatId)"
+    }
+}
+
+internal fun Chat(
+    chatId: String
+): Chat = ChatImpl(
+    chatId = chatId
+)
