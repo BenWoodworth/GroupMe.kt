f7197af04fd963ad14a298fb02e203ef2989653b
BenWoodworth <ben@benwoodworth.net>
2019-12-23 16:39:58 -0500

    Merge User.kt and User.impl.kt

--- src/main/kotlin/net/benwoodworth/groupme/User.kt
+++ src/main/kotlin/net/benwoodworth/groupme/User.kt
@@ -12,3 +12,25 @@ interface User {
 
     override fun toString(): String
 }
+
+private class UserImpl(
+    override val userId: String
+) : User {
+    override fun equals(other: Any?): Boolean {
+        return other is User && userId == other.userId
+    }
+
+    override fun hashCode(): Int {
+        return userId.hashCode()
+    }
+
+    override fun toString(): String {
+        return "User($userId)"
+    }
+}
+
+fun User(
+    userId: String
+): User = UserImpl(
+    userId = userId
+)
